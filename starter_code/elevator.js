c454e750ff6a8a39f17d05fc66d56566ce948f8d
Enrique Hernández <l6cenrique@gmail.com>
2018-01-29 20:04:40 +0100

    done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,92 @@
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.direction = '';
+    this.move;
+    this.waitingList = [];
+    this.passengers = [];
+
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    this.move = setInterval(() => { this.update() }, 1000);
+  }
+  stop() {
+    
+      clearInterval(this.move);
+
+  }
+  update() {
+    if (this.direction == "up") {
+      this.floorUp();
+      this._passengersEnter();
+      this._passengersLeave();
+    } else {
+      this.floorDown();
+      this._passengersLeave();
+      this._passengersEnter();
+    }
+    this.log();
+    
+   
+  }
+  _passengersEnter() {
+    let i = 0;
+    for (; i < this.waitingList.length; i++) {
+      if (this.waitingList[i].originFloor == this.floor) {
+        this.passengers.push(this.waitingList[i])
+        this.requests.push(this.waitingList[i].originFloor);
+        console.log(this.waitingList[i].name + " has enter the elevator")
+        this.waitingList.splice(i, 1);
+      }
+    }
+  }
+  _passengersLeave() {
+    let i = 0;
+    for (; i < this.passengers.length; i++) {
+      if (this.passengers[i].destinationFloor == this.floor) {
+        
+        console.log(this.passengers[i].name + " has left the elevator")
+        this.passengers.splice(i, 1);
+
+      }
+    }
+
+  }
+  floorUp() {
+
+    if (this.floor < this.MAXFLOOR) {
+      this.floor++;
+    }
+    else {
+      this.direction = "down"
+    }
+
+  }
+  floorDown() {
+
+    if (this.floor > 0) {
+      this.floor--;
+    } else {
+      this.direction = "up";
+    }
+  }
+  call(persona) {
+    this.requests.push(persona.originFloor);
+    this.waitingList.push(persona);
+
+
+  }
+  log() {
+    console.log("Direction : " + this.direction + " | Floor : " + this.floor);
+  }
 }
+
+
+
+
+
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,12 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+const ascensor = new Elevator();
+const jaime = new Person("Jaime", 2, 5);
+const julia = new Person("Julia", 6, 3);
+const perico = new Person("Perico", 10, 4);
+
+ascensor.call(julia);
+ascensor.call(jaime);
+ascensor.call(perico);
+ascensor.start();
+setTimeout(() => ascensor.stop(), 24000);
